5cd4426f855d4f65fed5a51e9c28dd75a07cad2f
Dmitry Kuznetsov <dkuznetsov@yandex.ru>
2017-07-15 12:34:48 +0200

    some osx tweaks

---
+++ osx/tweaks.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Finder
+#
+
+# show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# don't show removable media, disks and mounted servers on the Desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+
+# always use "View -> as List"
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+
+# Safari
+#
+
+# set Safari's home page to 'about:blank'
+defaults write com.apple.Safari HomePage -string "about:blank"
