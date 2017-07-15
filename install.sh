0aba70e42645e6b470f07cf3939b405b7ead8f5e
Dmitry Kuznetsov <dkuznetsov@yandex.ru>
2017-07-15 21:03:37 +0200

    bash dot files

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -sfv "$SCRIPT_DIR/vim/.vim" ~
 ln -sfv "$SCRIPT_DIR/vim/.vimrc" ~
 
+# Shell
+#
+if [ -f "~/.profile" ]; then
+    rm "~/.profile"
+fi
+ln -sfv "$SCRIPT_DIR/shell/bashrc" ~/.bashrc
+ln -sfv "$SCRIPT_DIR/shell/bash_profile" ~/.bash_profile
+ln -sfv "$SCRIPT_DIR/shell/bash_prompt" ~/.bash_prompt
+
