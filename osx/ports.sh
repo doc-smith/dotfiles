ad17e28fd1ce0099138df96057273fa0e5891920
Dmitry Kuznetsov <dkuznetsov@yandex.ru>
2017-07-15 21:04:04 +0200

    port installation

---
+++ osx/ports.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+if ! [ -x "$(command -v port)" ]; then
+    >&2 echo "Error: MacPorts are not installed."
+    exit 1
+fi
+
+sudo port install bash +completion
+BASH_PATH=`which bash`
+if ! grep "$BASH_PATH" /etc/shells; then
+    echo "$BASH_PATH" | sudo tee -a /etc/shells
+fi
+
+sudo port install python27 +readline
+sudo port select --set python python27
+sudo port select --set python2 python27
+
+sudo port install py27-pip
+sudo port select --set pip pip27
+
+sudo port install py27-virtualenv
+sudo port select --set virtualenv virtualenv27
+
