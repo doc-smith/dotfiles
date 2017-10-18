4ca3196a7633ff48f064c94fc8bb4d75ddb6e2fb
Dmitry Kuznetsov <dkuznetsov@yandex.ru>
2017-10-18 11:05:01 +0200

    os x specific useful functions in bashrc

--- install.sh
+++ install.sh
@@ -27,6 +27,11 @@ ln -sfv "$SCRIPT_DIR/shell/bashrc" ~/.bashrc
 ln -sfv "$SCRIPT_DIR/shell/bash_profile" ~/.bash_profile
 ln -sfv "$SCRIPT_DIR/shell/bash_prompt" ~/.bash_prompt
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ln -sfv "${SCRIPT_DIR}/shell/bashrc.osx" ~/.bashrc.osx
+fi
+
+
 
 # git
 #
