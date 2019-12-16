593794b3711d8ac88d16f0f765bddcbfdb206259
Dmitry Kuznetsov <dkuznetsov@yandex.ru>
2019-12-16 20:52:41 +0100

    install scheme

--- osx/install-tools.sh
+++ osx/install-tools.sh
@@ -13,11 +13,10 @@ stderr "Installing other tools..."
 stderr "Installing nvm..."
 if [ ! -d "${NVM_DIR}" ]; then
     export NVM_DIR="$HOME/.nvm" && (
-    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-    pushd "$NVM_DIR"
-    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+        git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+        cd "$NVM_DIR"
+        git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
     ) && \. "$NVM_DIR/nvm.sh"
-    popd
 else
     stderr "${NVM_DIR} already defined, nothing to install"
 fi
@@ -39,4 +38,5 @@ USEFUL_PORTS=(
     unzip
     jq
     tldr-cpp-client
+    mit-scheme
 )
